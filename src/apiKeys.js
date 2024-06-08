10b28db195cd40a9033b9ed01a6a8737b68310f8
Priyanshu Kumar <110731419+PRIYANSHUKUMAR123451@users.noreply.github.com>
2024-06-08 13:03:28 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "09bd817b2048f30c0110d346ad4edc62",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
